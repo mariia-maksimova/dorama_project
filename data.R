0f92be1c5bb6df3ef5320b5d2e36dbc6d5b6cb12
Veronica Porozova <nika.porozov@gmail.com>
2017-04-15 11:35:03 +0300

    1

--- data.R
+++ data.R
@@ -1,17 +1,12 @@
 library(readr)
-KR_genres <- read_delim("~/dorama_project/KR_genres.csv", 
-                        +     "\t", escape_double = FALSE, col_names = FALSE, 
-                        +     trim_ws = TRUE)
+KR_genres <- read_delim("~/dorama_project/KR_genres.csv", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
 colnames(KR_genres)<-c('genre_id', 'genre_title')
 
-KR_rates <- read_delim("~/dorama_project/KR_rates.csv", 
-                       +     "\t", escape_double = FALSE, col_names = FALSE, 
-                       +     trim_ws = TRUE)
+KR_rates <- read_delim("~/dorama_project/KR_rates.csv", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
 
 colnames(KR_rates)<-c('user_id', 'dorama_id', 'status', 'rating')
 
-KR_shows <- read_delim("~/dorama_project/KR_shows.csv", 
-                       +     "\t", escape_double = FALSE, col_names = FALSE, 
-                       +     trim_ws = TRUE)
+KR_shows <- read_delim("~/dorama_project/KR_shows.csv", "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE)
 
 colnames(KR_shows)<-c('dorama_id', 'dorama_title', 'overall_rating', 'watching', 'seasons', 'year', 'genres')
+
