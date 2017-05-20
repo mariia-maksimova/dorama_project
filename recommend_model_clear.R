1d89281941f4bf94f3665731500be47279143a76
Sabina Gasanova <sbgasanova@edu.hse.ru>
2017-05-20 14:23:34 +0300

    changes

--- recommend_model_clear.R
+++ recommend_model_clear.R
@@ -18,7 +18,7 @@ load('recc_model.RData')
 
 user_data<-matrix(data=NA, ncol = ncol(matrix_rates))
 colnames(user_data)<-colnames(matrix_rates)
-
+user_data<-t(user_data)
 ###Любимые жанры пользователя!!! Стыковка##
 
 user_genres=as.data.frame(matrix(nrow=1, ncol=5))
@@ -30,23 +30,30 @@ user_genres[5]=36
 
 ###
 
-for (i in 1:5){}
-#Вывод по пять дорам на каждый из 5 жанр (смотрел-не смотрел)
+dramas_matrix_work<-filter(dramas_matrix, is.element(genre_id,user_genres)==TRUE)
+first_page_dramas<-data.frame(dorama_id=stack(dramas_matrix_work[,2:6])[,1])
+first_page_dramas2<-filter(KR_shows, is.element(dorama_id,first_page_dramas$dorama_id)==TRUE )
 
 #вставляем оценки пользователя!!Стыковка!!
-user_data[43]=2
-user_data[443]=5
-user_data[775]=3
-user_data[413]=4
-user_data[13]=1
-user_data[32]=5
-user_data[100]=5
-user_data[323]=2
-user_data[578]=5
-user_data[143]=5
-user_data[493]=5
-user_data[432]=5
-user_data[1]=5
+
+for (i in first_page_dramas2$dorama_id){
+  user_data[rownames(user_data)==i]=5
+}
+user_data<-t(user_data)
+
+#user_data[43]=2
+#user_data[443]=5
+#user_data[775]=3
+#user_data[413]=4
+#user_data[13]=1
+#user_data[32]=5
+#user_data[100]=5
+#user_data[323]=2
+#user_data[578]=5
+#user_data[143]=5
+#user_data[493]=5
+#user_data[432]=5
+#user_data[1]=5
 ###Стыковка
 
 
@@ -54,7 +61,7 @@ user_data<-as(user_data,"realRatingMatrix")
 
 #рекомендация для нашего - newdata - его строка
 
-recc_predicted <- predict(object = recc_model, newdata = user_data, n = 50)
+recc_predicted <- predict(object = recc_model, newdata = user_data, n = 300)
 recc_user_1 <- recc_predicted@items[[1]]
 recc_user_ratings<-recc_predicted@ratings[[1]]
 
@@ -64,7 +71,21 @@ dorama_ids<-cbind(dorama_id, data.frame(rating=recc_user_ratings))
 library(dplyr)
 
 KR_rating_full$id=as.numeric(rownames(KR_rating_full))
-dramas=left_join(dorama_ids, KR_rating_full, by='dorama_id')
+KR_rating_full2<-KR_rating_full
+
+#m <- gregexpr('[[:digit:]]+', KR_rating_full$genres[1:1248], perl=TRUE)
+#genres=regmatches(KR_rating_full$genres[1:1248], m)
+#genres<-sapply( genres, as.numeric )
+
+#for (each in KR_rating_full2$id){
+#  if (all(is.element(genres[[each]], user_genres))==FALSE){
+#    KR_rating_full2[each,]=NA
+#  }
+#}
+#KR_rating_full2<-na.omit(KR_rating_full2)
+
+dramas=inner_join(dorama_ids, KR_rating_full2, by='dorama_id')
+
 
 #Вывод дорам отфильтрованных по жанру (та же самая матрица и смотрится, чтобы хотя б один жанры каждой дорамы был в user_genres)
 
@@ -72,20 +93,31 @@ dramas=left_join(dorama_ids, KR_rating_full, by='dorama_id')
 user_wants=as.data.frame(matrix(nrow=1, ncol=10))
 colnames(user_wants)<-c('School',	'History',	'Gender',	'Super',	'Pro',	'Investigation',	'Cinderella',	'Revenge','Friendship','Secrets')
 
-user_wants$School=7
+user_wants$School=4
 user_wants$History=3
 user_wants$Gender=5
 user_wants$Super=2
-user_wants$Pro=8
+user_wants$Pro=5
 user_wants$Investigation=4
-user_wants$Cinderella=6
-user_wants$Revenge=9
-user_wants$Friendship=10
-user_wants$Secrets=9
+user_wants$Cinderella=4
+user_wants$Revenge=6
+user_wants$Friendship=5
+user_wants$Secrets=4
 
 ###
 
 #После каждого ОК Строится новая таблица drama_filter, в которой есть только дорамы +-1 отличающиеся по оценке и они выводятся
+
+dramas_wants<-filter(dramas, School>=user_wants$School-2, School<=user_wants$School+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$History-2, School<=user_wants$History+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Gender-2, School<=user_wants$Gender+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Super-2, School<=user_wants$Super+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Pro-2, School<=user_wants$Pro+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Investigation-2, School<=user_wants$Investigation+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Cinderella-2, School<=user_wants$Cinderella+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Revenge-2, School<=user_wants$Revenge+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Friendship-2, School<=user_wants$Friendship+2)
+dramas_wants<-filter(dramas_wants, School>=user_wants$Secrets-2, School<=user_wants$Secrets+2)
 
 
 
